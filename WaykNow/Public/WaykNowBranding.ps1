0bc84e689e085ba8b880b2711a07b4f4547328d6
Johann Dufaud <jdufaud@devolutions.net>
2019-09-30 11:20:50 -0400

    Moved method Get-FileEncoding To Private folder in helper

--- WaykNow/Public/WaykNowBranding.ps1
+++ WaykNow/Public/WaykNowBranding.ps1
@@ -125,28 +125,4 @@ function Test-WaykNowBranding(
     }
 }
 
-function Get-FileEncoding(
-    [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)]
-    [string]$Path
-)
-{
-    [byte[]]$byte = get-content -Encoding byte -ReadCount 4 -TotalCount 4 -Path $Path
-
-    if ( $byte[0] -eq 0xef -and $byte[1] -eq 0xbb -and $byte[2] -eq 0xbf ){ 
-        Write-Output 'UTF8-BOM' 
-    }
-    elseif ($byte[0] -eq 0xfe -and $byte[1] -eq 0xff){
-        Write-Output 'Unicode' 
-    }
-    elseif ($byte[0] -eq 0 -and $byte[1] -eq 0 -and $byte[2] -eq 0xfe -and $byte[3] -eq 0xff){ 
-        Write-Output 'UTF32' 
-    }
-    elseif ($byte[0] -eq 0x2b -and $byte[1] -eq 0x2f -and $byte[2] -eq 0x76){
-        Write-Output 'UTF7'
-    }
-    else{
-        Write-Output 'UTF8-NOBOM'
-    }
-}
-
 Export-ModuleMember -Function Set-WaykNowBranding, Reset-WaykNowBranding, Test-WaykNowBranding
