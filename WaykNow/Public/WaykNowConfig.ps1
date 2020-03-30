77438616b3d84d7afc975770c270005d49c504be
Marc-André Moreau <mamoreau@devolutions.net>
2020-03-30 17:01:25 -0400

    WaykNow-ps: start refactoring Set-WaykNowConfig/Get-WaykNowConfig

--- WaykNow/Public/WaykNowConfig.ps1
+++ WaykNow/Public/WaykNowConfig.ps1
@@ -4,9 +4,9 @@
 
 enum ControlMode 
 {
-    AllRemoteControlMode = 0
-    TakeRemoteControlClientOnly = 1
-    AllowRemoteControlServerOnly = 2
+    Both = 0
+    Client = 1
+    Server = 2
 }
 
 enum PersonalPasswordType 
@@ -51,143 +51,162 @@ class WaykNowConfig
 	# General
     [string] $FriendlyName
     [string] $Language
-    [ControlMode] $ControlMode
-    [string] $AutoLaunchOnUserLogon
-    [string] $ShowMainWindowOnLaunch
-    [string] $MinimizeToNotificationArea
-    [string] $ElevationPrompt
+    [ControlMode] $ControlMode = [ControlMode]::Both
+    [bool] $AutoLaunchOnUserLogon = $false
+    [bool] $ShowMainWindowOnLaunch = $true
+    [bool] $MinimizeToNotificationArea = $false
+    [bool] $ElevationPrompt = $false
 
     # Security
-    [string] $AllowPersonalPassword
-    [string] $AllowSystemAuth
-    [string] $AllowNoPassword
-    [PersonalPasswordType]  $PersonalPasswordType
+    [bool] $AllowPersonalPassword = $true
+    [bool] $AllowSystemAuth = $true
+    [bool] $AllowNoPassword = $true
+    [PersonalPasswordType] $PersonalPasswordType
     [string] $PersonalPassword
-    [int32] $GeneratedPasswordLength
-    [GeneratedPasswordCharSet] $GeneratedPasswordCharSet
+    [int32] $GeneratedPasswordLength = 6
+    [bool] $GeneratedPasswordAutoReset = $true
+    [GeneratedPasswordCharSet] $GeneratedPasswordCharSet = [GeneratedPasswordCharSet]::Alphanumeric
 
     # Connectivity
-    [string] $DenEnabled
-    [string] $DenUrl
+    [bool] $DenEnabled = $true
+    [string] $DenUrl = "https://den.wayk.net"
 
     # Advanced
-    [QualityMode] $QualityMode
+    [QualityMode] $QualityMode = [QualityMode]::Medium
     [LoggingLevel] $LoggingLevel
     [string] $LoggingFilter
 
     # Access Control
-    [AccessControl] $AccessControlViewing
-    [AccessControl] $AccessControlInteract
-    [AccessControl] $AccessControlClipboard
-    [AccessControl] $AccessControlFileTransfer
-    [AccessControl] $AccessControlExec
-    [AccessControl] $AccessControlChat
+    [AccessControl] $AccessControlViewing = [AccessControl]::Allow
+    [AccessControl] $AccessControlInteract = [AccessControl]::Allow
+    [AccessControl] $AccessControlClipboard = [AccessControl]::Allow
+    [AccessControl] $AccessControlFileTransfer = [AccessControl]::Allow
+    [AccessControl] $AccessControlExec = [AccessControl]::Allow
+    [AccessControl] $AccessControlChat = [AccessControl]::Allow
+
+    [bool] $AnalyticsEnabled = $true
+    [bool] $CrashReporterEnabled = $true
+    [bool] $CrashReporterAutoUpload = $true
+    [bool] $VersionCheck = $true
 }
 
-<#
-    .SYNOPSIS
-        The `Set-WaykNowConfig` command, is used for modfy multiple settings from WaykNow.
-#>
-function Set-WaykNowConfig
-(
-    # Use global config file rather than user config file.
-    [switch] $Global,
-
-    # The Friendly Name is used for Prompt For Permission (PFP) authentication. It should be easily recognized by your peers. 
-    [string] $FriendlyName,
-
-    # Specifies the language of the application, "en" for English, "fr" for French, "de" German, "zh-CN" for Chinese Simplified, "zh-TW" for Chinese Traditional.
-    [ValidateSet("en", "fr", "de", "zh-CN", "zh-TW")]
-    [string] $Language,
-
-    # Specifies the Remote Control Mode of WaykNow, AllRemoteControlMode: Both sides are displayed, TakeRemoteControlClientOnly: Only the client side is displayed and AllowRemoteControlServerOnly: Only the server side is displayed.    [ControlMode] $ControlMode,
-    [ControlMode] $ControlMode,
-
-    # Launch Wayk Now when you log on.
-    [ValidateSet("true", "false")]
-    [string] $AutoLaunchOnUserLogon,
-
-    # Prevent the main application window from showing when Wayk Now starts. It can be quite useful when the application is automatically launched.
-    [ValidateSet("true", "false")]
-    [string] $ShowMainWindowOnLaunch,
-
-    # Hide Wayk Now from the taskbar when minimized.
-    [ValidateSet("true", "false")]
-    [string] $MinimizeToNotificationArea,
-
-    # Disable the prompt to elevate program permissions, and run Wayk Now without elevated program permissions.
-    [ValidateSet("true", "false")]
-    [string] $ElevationPrompt,
-
-    # Enabled/disabled SRP: When Secure Remote Password is disabled, the password options are disabled as well.
-    [ValidateSet("true", "false")]
-    [string] $AllowPersonalPassword,
-
-    # Setting to enabled/disabled SRD: Secure Remote Delegation is the method used for system authentication in the case of unattended remote access. On Windows, remote access is restricted to members of the built-in Administrators or Remote Desktop Users groups.
-    [ValidateSet("true", "false")]
-    [string] $AllowSystemAuth,
-
-    # Setting to enabled/disabled PFP: Prompt for Permission authentication requests explicit consent from the remote user without the need for a password.
-    [ValidateSet("true", "false")]
-    [string] $AllowNoPassword,
-
-    # Select your password type: 
-    # - Generated Password 
-    # Generate a strong, random password with our password generator which can be configured with the -GeneratedPasswordLength and -GeneratedPasswordCharSet section. 
-    # - Custom Password 
-    #Create a custom password of your own choosing.
-    [PersonalPasswordType]  $PersonalPasswordType,
-
-    #Create a custom password of your own choosing.
-    [string] $PersonalPassword,
-
-    # The generated password length, between 3 and 9
-    [int32] $GeneratedPasswordLength,
-
-    # The parameter used by the password generator:
-    # The alphanumeric character set contains numbers and letters, excluding 0, O, 1, I for a total of 32 characters. This choice was made to avoid any possible confusion when communicating the password to the other user.
-    [GeneratedPasswordCharSet] $GeneratedPasswordCharSet,
-
-    # Connect to Wayk Den to enable simplified peer-to-peer connectivity with a 6-digit ID.
-    [ValidateSet("true", "false")]
-    [string] $DenEnabled,
-
-    # Connect to the Wayk Den server with the URL
-    [string] $DenUrl,
-
-    # The quality mode allow to adjust the quality of the render to optimize performance.
-    [QualityMode] $QualityMode,
-
-    # This Logging level option affects the verbosity of the logging messages.
-    [LoggingLevel] $LoggingLevel,
-
-    # This Logging filter option filters the types of messages that are logged.
-    # Do not use unless instructed.
-    [string] $LoggingFilter,
-
-    # The Access Control section allows you to restrict access to certain resources shared by the server. In other words, access control defines what can be done to your machine when someone else is connected. You can set each feature independently.
-    #- Allow: The feature is enabled.
-    #- Confirm: The feature is disabled, but can be enabled after user confirmation during the session.
-    #- Disable: The feature is disabled. For security reasons or to enforce company policies, you may want to disable specific features.
-    # The viewing access control
-    [AccessControl] $AccessControlViewing,
-
-    # The interaction access control
-    [AccessControl] $AccessControlInteract,
-
-    # The clipboard access control
-    [AccessControl] $AccessControlClipboard,
-
-    # The file transfer access control
-    [AccessControl] $AccessControlFileTransfer,
-
-    # The execution access control
-    [AccessControl] $AccessControlExec,
-
-    # The chat access control
-    [AccessControl] $AccessControlChat
-)
+function Get-WaykNowConfigFile
 {
+    param(
+        [switch] $Global
+    )
+
+    [WaykNowInfo]$WaykInfo = Get-WaykNowInfo
+
+    if ($Global) {
+        $ConfigFile = $WaykInfo.GlobalConfigFile
+    } else {
+        $ConfigFile = $WaykInfo.ConfigFile
+    }
+
+    return $ConfigFile
+}
+
+function Set-WaykNowConfig
+{
+    [CmdletBinding()]
+    param(
+        # Use global config file rather than user config file.
+        [switch] $Global,
+
+        # The Friendly Name is used for Prompt For Permission (PFP) authentication. It should be easily recognized by your peers. 
+        [string] $FriendlyName,
+
+        # Specifies the language of the application, "en" for English, "fr" for French, "de" German, "zh-CN" for Chinese Simplified, "zh-TW" for Chinese Traditional.
+        [ValidateSet("en", "fr", "de", "zh-CN", "zh-TW")]
+        [string] $Language,
+
+        # Specifies the Remote Control Mode of WaykNow, AllRemoteControlMode: Both sides are displayed, TakeRemoteControlClientOnly: Only the client side is displayed and AllowRemoteControlServerOnly: Only the server side is displayed.    [ControlMode] $ControlMode,
+        [ControlMode] $ControlMode,
+
+        # Launch Wayk Now when you log on.
+        [bool] $AutoLaunchOnUserLogon,
+
+        # Prevent the main application window from showing when Wayk Now starts. It can be quite useful when the application is automatically launched.
+        [bool] $ShowMainWindowOnLaunch,
+
+        # Hide Wayk Now from the taskbar when minimized.
+        [bool] $MinimizeToNotificationArea,
+
+        # Disable the prompt to elevate program permissions, and run Wayk Now without elevated program permissions.
+        [bool] $ElevationPrompt,
+
+        # Enabled/disabled SRP: When Secure Remote Password is disabled, the password options are disabled as well.
+        [bool] $AllowPersonalPassword,
+
+        # Setting to enabled/disabled SRD: Secure Remote Delegation is the method used for system authentication in the case of unattended remote access. On Windows, remote access is restricted to members of the built-in Administrators or Remote Desktop Users groups.
+        [bool] $AllowSystemAuth,
+
+        # Setting to enabled/disabled PFP: Prompt for Permission authentication requests explicit consent from the remote user without the need for a password.
+        [bool] $AllowNoPassword,
+
+        # Select your password type: 
+        # - Generated Password 
+        # Generate a strong, random password with our password generator which can be configured with the -GeneratedPasswordLength and -GeneratedPasswordCharSet section. 
+        # - Custom Password 
+        #Create a custom password of your own choosing.
+        [PersonalPasswordType]  $PersonalPasswordType,
+
+        #Create a custom password of your own choosing.
+        [string] $PersonalPassword,
+
+        # The generated password length, between 3 and 9
+        [int32] $GeneratedPasswordLength,
+
+        # The parameter used by the password generator:
+        # The alphanumeric character set contains numbers and letters, excluding 0, O, 1, I for a total of 32 characters. This choice was made to avoid any possible confusion when communicating the password to the other user.
+        [GeneratedPasswordCharSet] $GeneratedPasswordCharSet,
+
+        # Connect to Wayk Den to enable simplified peer-to-peer connectivity with a 6-digit ID.
+        [bool] $DenEnabled,
+
+        # Connect to the Wayk Den server with the URL
+        [string] $DenUrl,
+
+        # The quality mode allow to adjust the quality of the render to optimize performance.
+        [QualityMode] $QualityMode,
+
+        # This Logging level option affects the verbosity of the logging messages.
+        [LoggingLevel] $LoggingLevel,
+
+        # This Logging filter option filters the types of messages that are logged.
+        # Do not use unless instructed.
+        [string] $LoggingFilter,
+
+        # The Access Control section allows you to restrict access to certain resources shared by the server. In other words, access control defines what can be done to your machine when someone else is connected. You can set each feature independently.
+        #- Allow: The feature is enabled.
+        #- Confirm: The feature is disabled, but can be enabled after user confirmation during the session.
+        #- Disable: The feature is disabled. For security reasons or to enforce company policies, you may want to disable specific features.
+        # The viewing access control
+        [AccessControl] $AccessControlViewing,
+
+        # The interaction access control
+        [AccessControl] $AccessControlInteract,
+
+        # The clipboard access control
+        [AccessControl] $AccessControlClipboard,
+
+        # The file transfer access control
+        [AccessControl] $AccessControlFileTransfer,
+
+        # The execution access control
+        [AccessControl] $AccessControlExec,
+
+        # The chat access control
+        [AccessControl] $AccessControlChat,
+
+        # Automatically upload crash reports
+        [bool] $CrashReporterAutoUpload,
+
+        # Check for new version on application start
+        [bool] $VersionCheck
+    )
+
     $WaykInfo = Get-WaykNowInfo
 
     if (Test-Path $WaykInfo.ConfigFile) {
@@ -277,7 +296,7 @@ function Set-WaykNowConfig
         $json = Set-JsonValue $json 'DenUrl' $DenUrl
     }
 
-    #Advanced
+    # Advanced
     if ($null -ne $QualityMode) {
         $json = Set-JsonValue $json 'QualityMode' $QualityMode
     }
@@ -290,6 +309,22 @@ function Set-WaykNowConfig
         $json = Set-JsonValue $json 'LoggingFilter' $LoggingFilter
     }
 
+    if ($CrashReporterAutoUpload) {
+        $json = Set-JsonValue $json 'CrashReporterAutoUpload' $CrashReporterAutoUpload
+    }
+
+    if ($VersionCheck) {
+        $json = Set-JsonValue $json 'VersionCheck' $VersionCheck
+    }
+
+    $properties = [WaykNowConfig].GetProperties() | ForEach-Object { $_.Name }
+    foreach ($param in $PSBoundParameters.GetEnumerator()) {
+        if ($properties -Contains $param.Key) {
+            Write-Host "$($param.Key) = $($param.Value)"
+            $json = Set-JsonValue $json $param.Key $param.Value
+        }
+    }
+
     $AccessControl = [pscustomobject]@{
         'Viewing' = $AccessControlViewing
         'Interact' = $AccessControlInteract
@@ -300,7 +335,7 @@ function Set-WaykNowConfig
     }
 
     # To ignore the null value on the json we remove the values who are not set if there are not in the json file
-    #Access Control
+    # Access Control
     if ($null -eq $AccessControlViewing) {
         if ($json.AccessControl.Viewing)
         {
@@ -375,15 +410,44 @@ function Set-WaykNowConfig
         $ConfigFile = $WaykInfo.ConfigFile
     }
 
-    New-Item -Path $(Split-Path $ConfigFile -Parent) -ItemType 'Directory' -Force
+    New-Item -Path $(Split-Path $ConfigFile -Parent) -ItemType 'Directory' -Force | Out-Null
 
     $fileValue = $json | ConvertTo-Json
     $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
     [System.IO.File]::WriteAllLines($ConfigFile, $fileValue, $Utf8NoBomEncoding)
 }
 
-function Get-WaykNowConfig()
+function Get-WaykNowConfigInternal
 {
+    [CmdletBinding()]
+    [OutputType('WaykNowConfig')]
+    param(
+        [switch] $Global
+    )
+
+    $ConfigFile = Get-WaykNowConfigFile -Global:$Global
+
+    $json = Get-Content -Path $ConfigFile -Raw | ConvertFrom-Json
+
+    $config = [WaykNowConfig]::new()
+    [WaykNowConfig].GetProperties() | ForEach-Object {
+        $Name = $_.Name
+        $Property = $json.PSObject.Properties[$Name]
+        if ($Property) {
+            $Value = $Property.Value
+            $config.$Name = $Value
+        }
+    }
+
+    return $config
+}
+
+function Get-WaykNowConfig
+{
+    [CmdletBinding()]
+    [OutputType('WaykNowConfig')]
+    param()
+
     [WaykNowInfo]$WaykInfo = Get-WaykNowInfo
 
     $GlobalServiceAvailable = $false;
@@ -424,6 +488,8 @@ function Get-WaykNowConfig()
     $WaykNowConfigObject.AccessControlFileTransfer = Get-NowOptionInt 'AccessControl.FileTransfer' $GlobalServiceAvailable $true ([int]([AccessControl]::Allow)) $LocalJson $GlobalJson
     $WaykNowConfigObject.AccessControlExec = Get-NowOptionInt 'AccessControl.Exec' $GlobalServiceAvailable $true ([int]([AccessControl]::Allow)) $LocalJson $GlobalJson
     $WaykNowConfigObject.AccessControlChat = Get-NowOptionInt 'AccessControl.Chat' $GlobalServiceAvailable $true ([int]([AccessControl]::Allow)) $LocalJson $GlobalJson
+    $WaykNowConfigObject.CrashReporterAutoUpload = (Get-NowOptionBool 'CrashReporterAutoUpload' $GlobalServiceAvailable $true $true $LocalJson $GlobalJson)
+    $WaykNowConfigObject.VersionCheck = (Get-NowOptionBool 'VersionCheck' $GlobalServiceAvailable $true $true $LocalJson $GlobalJson)
 
     return $WaykNowConfigObject
 }
--- pester/Config.Tests.ps1
+++ pester/Config.Tests.ps1
@@ -7,14 +7,18 @@ Describe 'Wayk Now config' {
 
 		Context 'Empty configuration files' {
 			It 'Disables Prompt for Permission (PFP)' {
-				Set-WaykNowConfig -AllowPersonalPassword false
-				$(Get-WaykNowConfig).AllowPersonalPassword | Should -Be false
+				Set-WaykNowConfig -Global -AllowPersonalPassword $false
+				$(Get-WaykNowConfig).AllowPersonalPassword | Should -Be $false
 				Assert-MockCalled 'Get-WaykNowPath'
 			}
 			It 'Sets server-only remote control mode' {
-				Set-WaykNowConfig -ControlMode AllowRemoteControlServerOnly
+				Set-WaykNowConfig -Global -ControlMode AllowRemoteControlServerOnly
 				$(Get-WaykNowConfig).ControlMode | Should -Be 'AllowRemoteControlServerOnly'
 				Assert-MockCalled 'Get-WaykNowPath'
+			}
+			It 'Disables the version check' {
+				Set-WaykNowConfig -Global -VersionCheck $false
+				$(Get-WaykNowConfig).VersionCheck | Should -Be $false
 			}
 		}
 	}
